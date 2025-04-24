f9c69557147b4fcf79a76a9cacab28d0e91b90b0
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2025-04-25 01:49:14 +0200

    feat: project mcknowledgebook

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,31 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://mcknowledgebook.vercel.app',
    integrations: [
       starlight({
          title: 'McKnowledgeBook',
-         social: [],
-         sidebar: [],
+         favicon: '/favicon.ico',
+         logo: {
+            src: './src/assets/favicon.webp'
+         },
+         description: 'The Minecraft modding knowledge book',
+         social: [
+            {
+               label: 'GitHub',
+               href: 'https://github.com/modpackker/mcknowledgebook',
+               icon: 'github'
+            }
+         ],
+         sidebar: [
+            'overview',
+         ],
+         editLink: {
+            baseUrl: 'https://github.com/modpackker/mcknowledgebook/edit/main/'
+         },
+         customCss: [
+            './src/styles/index.css'
+         ]
       }),
    ],
 });
