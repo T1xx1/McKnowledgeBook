e198adc61cfd04ce130046390a74d218cc6dccad
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2025-05-06 17:47:40 +0200

    feat: 25w19a snapshot

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,7 @@ export default defineConfig({
                   'news',
                   {
                      slug: 'versions',
-                     label: '25w18a snapshot',
+                     label: '25w19a snapshot',
                      badge: {
                         variant: 'tip',
                         text: 'New'
