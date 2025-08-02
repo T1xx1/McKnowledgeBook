2f1dfd418ca7abad3ddfb4828c2268dee14fa5b9
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-02 11:58:25 +0200

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,160 +6,160 @@ import starlightLinksValidator from 'starlight-links-validator';
 
 // https://astro.build/config
 export default defineConfig({
-   vite: {
-      plugins: [tailwindcss()]
-   },
-   site: 'https://mcknowledgebook.vercel.app',
-   integrations: [
-      starlight({
-         plugins: [
-            starlightLinksValidator()
-         ],
-         title: 'McKnowledgeBook',
-         favicon: '/favicon.ico',
-         logo: {
-            src: './src/assets/favicon.webp'
-         },
-         description: 'The Minecraft modding Wiki.',
-         social: [
-            {
-               icon: 'blueSky',
-               label: 'BlueSky',
-               href: 'https://bsky.app/profile/modpackker.bsky.social'
-            },
-            {
-               icon: 'discord',
-               label: 'Discord',
-               href: 'https://discord.gg/gaDqYhxyzB'
-            },
-            {
-               icon: 'github',
-               label: 'GitHub',
-               href: 'https://github.com/modpackker/mcknowledgebook'
-            },
-            {
-               icon: 'instagram',
-               label: 'Instagram',
-               href: 'https://instagram.com/modpackker'
-            },
-            {
-               icon: 'threads',
-               label: 'Threads',
-               href: 'https://threads.net/@modpackker'
-            }
-         ],
-         sidebar: [
-            {
-               label: 'News',
-               items: [
-                  'news',
-				  {
-					 slug: 'versions',
-					 label: '25w31a',
-					 badge: {
-						variant: 'tip',
-						text: 'New'
-					 }
-				  },
-                  {
-                     slug: 'chasetheskies',
-                     badge: {
-                        variant: 'tip',
-                        text: 'New'
-                     }
-                  },
-                  {
-                     slug: 'dialogcommand',
-                     badge: {
-                        variant: 'tip',
-                        text: 'New'
-                     }
-                  },
-                  {
-                     slug: 'vibrantvisuals',
-                     badge: {
-                        variant: 'tip',
-                        text: 'New'
-                     }
-                  }
-               ]
-            },
-            {
-               label: 'Minecraft',
-               items: [
-                  'minecraft',
-                  'vanilla',
-                  'environment',
-                  'edition',
-                  'versions',
-                  'vibrantvisuals',
-                  'commands',
-                  'resourcepack',
-                  'datapack',
-                  'minecraftmisc'
-               ]
-            },
-            {
-               label: 'Modding',
-               items: [
-                  'modding',
-                  'minecraftsobfuscatedcodebase',
-                  'moddingenvironment',
-                  'loader',
-                  'projecttype',
-                  'mod',
-                  'plugin',
-                  'shader',
-                  'moddingmisc',
-                  'modpack'
-               ]
-            },
-            {
-               label: 'McKnowledgeBook',
-               items: [
-                  'why',
-                  'tech'
-               ]
-            },
-            {
-               label: 'Modpackker',
-               link: 'https://modpackker.vercel.app',
-            },
-            {
-               label: '💚 T1xx1',
-               link: 'https://t1xx1.vercel.app',
-            }
-         ],
-         editLink: {
-            baseUrl: 'https://github.com/modpackker/mcknowledgebook/edit/main/'
-         },
-         head: [
-            {
-               tag: 'link',
-               attrs: {
-                  rel: 'preconnect',
-                  href: 'https://fonts.googleapis.com'
-               }
-            },
-            {
-               tag: 'link',
-               attrs: {
-                  rel: 'preconnect',
-                  href: 'https://fonts.gstatic.com',
-                  crossorigin: true
-               }
-            },
-            {
-               tag: 'link',
-               attrs: {
-                  rel: 'stylesheet',
-                  href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap'
-               }
-            }
-         ],
-         customCss: [
-            './src/styles/index.css'
-         ]
-      }),
-   ]
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	site: 'https://mcknowledgebook.vercel.app',
+	integrations: [
+		starlight({
+			plugins: [
+				starlightLinksValidator()
+			],
+			title: 'McKnowledgeBook',
+			favicon: '/favicon.ico',
+			logo: {
+				src: './src/assets/favicon.webp'
+			},
+			description: 'The Minecraft modding Wiki.',
+			social: [
+				{
+					icon: 'blueSky',
+					label: 'BlueSky',
+					href: 'https://bsky.app/profile/modpackker.bsky.social'
+				},
+				{
+					icon: 'discord',
+					label: 'Discord',
+					href: 'https://discord.gg/gaDqYhxyzB'
+				},
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/modpackker/mcknowledgebook'
+				},
+				{
+					icon: 'instagram',
+					label: 'Instagram',
+					href: 'https://instagram.com/modpackker'
+				},
+				{
+					icon: 'threads',
+					label: 'Threads',
+					href: 'https://threads.net/@modpackker'
+				}
+			],
+			sidebar: [
+				{
+					label: 'News',
+					items: [
+						'news',
+						{
+							slug: 'versions',
+							label: '25w31a',
+							badge: {
+								variant: 'tip',
+								text: 'New'
+							}
+						},
+						{
+							slug: 'chasetheskies',
+							badge: {
+								variant: 'tip',
+								text: 'New'
+							}
+						},
+						{
+							slug: 'dialogcommand',
+							badge: {
+								variant: 'tip',
+								text: 'New'
+							}
+						},
+						{
+							slug: 'vibrantvisuals',
+							badge: {
+								variant: 'tip',
+								text: 'New'
+							}
+						}
+					]
+				},
+				{
+					label: 'Minecraft',
+					items: [
+						'minecraft',
+						'vanilla',
+						'environment',
+						'edition',
+						'versions',
+						'vibrantvisuals',
+						'commands',
+						'resourcepack',
+						'datapack',
+						'minecraftmisc'
+					]
+				},
+				{
+					label: 'Modding',
+					items: [
+						'modding',
+						'minecraftsobfuscatedcodebase',
+						'moddingenvironment',
+						'loader',
+						'projecttype',
+						'mod',
+						'plugin',
+						'shader',
+						'moddingmisc',
+						'modpack'
+					]
+				},
+				{
+					label: 'McKnowledgeBook',
+					items: [
+						'why',
+						'tech'
+					]
+				},
+				{
+					label: 'Modpackker',
+					link: 'https://modpackker.vercel.app',
+				},
+				{
+					label: '💚 T1xx1',
+					link: 'https://t1xx1.vercel.app',
+				}
+			],
+			editLink: {
+				baseUrl: 'https://github.com/modpackker/mcknowledgebook/edit/main/'
+			},
+			head: [
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.googleapis.com'
+					}
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.gstatic.com',
+						crossorigin: true
+					}
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'stylesheet',
+						href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap'
+					}
+				}
+			],
+			customCss: [
+				'./src/styles/index.css'
+			]
+		}),
+	]
 });
