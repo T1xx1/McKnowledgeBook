cfd7ac2f9c9a26ef5cc06dce0075e26f6cbcf965
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-04 10:05:53 +0200

    change: resource pack and datapack versions interactive tables

---
+++ src/content/components/button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { children, onclick } = $props();
+</script>
+
+<button
+	type="button"
+	class="bg-primary hover:bg-primary/80 rounded-md px-3 py-1 cursor-pointer"
+	{onclick}
+>
+	{@render children()}
+</button>
---
+++ src/content/docs/datapack/version.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { versionsInfo } from '~/data/versions';
+
+	let { _ } = $props();
+
+	let versionInfo = $derived(versionsInfo.get(_));
+</script>
+
+{#if versionInfo}
+	<tr class="*:px-2 *:py-3">
+		<td>
+			<code>{_}</code>
+		</td>
+		<td>{versionInfo.datapackVersion}</td>
+	</tr>
+{:else}
+	<tr class="*:px-2 *:py-3">
+		<td colspan="2">
+			<code>{_}</code>
+		</td>
+	</tr>
+{/if}
