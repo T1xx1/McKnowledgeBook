f752e193febb81c5d36f0087fcbe1b54b14f3449
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-26 19:20:35 +0200

    feat: formatting codes page

--- astro.config.mjs
+++ astro.config.mjs
@@ -87,6 +87,7 @@ export default defineConfig({
 							items: [
 								'besttoolsenchantments',
 								'editionsdiffs',
+								'formattingcodes',
 								'unobtainableresources',
 								'unusedbedrockfeatures',
 								'unusedjavafeatures',
