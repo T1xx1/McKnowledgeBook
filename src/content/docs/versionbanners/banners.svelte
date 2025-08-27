f424e3c77d681e3565ac26d27e3469575b9ed9df
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-27 19:00:40 +0200

    feat: versions banner page

---
+++ src/content/docs/versionbanners/banners.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import {versionsInfo} from '~/data/versions.ts';
+</script>
+
+{#each versionsInfo as versionInfo}
+	<div>
+		<span class="text-xl">{versionInfo[0]}</span>
+		<img src={`https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/${versionInfo[0]}_1170x500.jpg`} alt={versionInfo[0]} />
+	</div>
+{/each}
