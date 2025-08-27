1b7046a93c07fe9f9806369f8ff7fe2d29ce74d4
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-27 20:16:50 +0200

    feat: all loader bindings

--- src/content/components/bindings.svelte
+++ src/content/components/bindings.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
-	import { mcVersions, mcMajorVersions, mcLegacyVersions } from '@mc/metadata';
+	import {
+		mcVersions,
+		mcMajorVersions,
+		mcLegacyVersions,
+		type Bindings,
+		type ProjectBindings,
+	} from '@mc/metadata';
 
 	import Button from '~/components/button.svelte';
 	import Switch from '~/components/switch.svelte';
 
-	import Version from './version.svelte';
+	let {
+		type = 'loader',
+		data,
+	}:
+		| {
+				type: 'loader';
+				data: Bindings;
+		  }
+		| {
+				type: 'project';
+				data: ProjectBindings;
+		  } = $props();
 
 	let showSnapshots = $state(false);
 	let showAll = $state(false);
@@ -39,12 +56,37 @@
 					<a href="/minecraft">Minecraft</a>
 					<a href="/versions">Version</a>
 				</th>
-				<th>Resource pack version</th>
+				{#if type === 'loader'}
+					<th>Binding</th>
+				{:else}
+					{#each Object.keys(data) as loader}
+						<th class="capitalize">{loader}</th>
+					{/each}
+				{/if}
 			</tr>
 
-			{#each viewVersions as version}
-				<Version _={version} />
-			{/each}
+			{#if type === 'loader'}
+				{#each viewVersions as version}
+					<tr>
+						<td>
+							<code>{version}</code>
+						</td>
+						<td>{data[version]}</td>
+					</tr>
+				{/each}
+			{:else}
+				{#each viewVersions as version}
+					<tr>
+						<td>
+							<code>{version}</code>
+						</td>
+
+						{#each Object.keys(data) as loader}
+							<td>{data[loader][version]}</td>
+						{/each}
+					</tr>
+				{/each}
+			{/if}
 		</tbody>
 	</table>
 
