ea0de0cd354c81829cb140edacd8b64890c01064
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-28 22:34:15 +0200

    change: use minecraft wiki version imgs

--- src/content/docs/versionbanners/banners.svelte
+++ src/content/docs/versionbanners/banners.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import {versionsInfo} from '~/data/versions.ts';
+	import { versionsInfo } from '~/data/versions.ts';
 </script>
 
 {#each versionsInfo as versionInfo}
 	<div>
 		<span class="text-xl">{versionInfo[0]}</span>
-		<img src={`https://minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/${versionInfo[0]}_1170x500.jpg`} alt={versionInfo[0]} />
+
+		{#if versionInfo[0].includes('w')}
+			<img src={`https://minecraft.wiki/images/${versionInfo[0]}.jpg`} alt={versionInfo[0]} />
+		{:else}
+			<img
+				src={`https://minecraft.wiki/images/${versionInfo[0]}_banner.jpg`}
+				alt={versionInfo[0]}
+			/>
+		{/if}
 	</div>
 {/each}
