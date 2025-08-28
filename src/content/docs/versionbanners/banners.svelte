585bef3760f3fe615b8900ddbf35f30e3a0f0a51
Tiziano <69322987+T1xx1@users.noreply.github.com>
2025-08-28 20:32:30 +0200

    chore: format

--- src/content/docs/versionbanners/banners.svelte
+++ src/content/docs/versionbanners/banners.svelte
@@ -5,6 +5,6 @@
 {#each versionsInfo as versionInfo}
 	<div>
 		<span class="text-xl">{versionInfo[0]}</span>
-		<img src={`https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/${versionInfo[0]}_1170x500.jpg`} alt={versionInfo[0]} />
+		<img src={`https://minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/${versionInfo[0]}_1170x500.jpg`} alt={versionInfo[0]} />
 	</div>
 {/each}
